5403ba9aa634acbf56f62620f81da3a3857e505f
idankario <idankario@gmail.com>
2021-11-11 20:43:53 +0200

    add idan

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const http = require ("http");
 const port = 8080;
 
 http.createServer((req, res) => {
-	res.writeHead(200); // Status code in header
-	res.write("Noam Roytman"); // Send res body (can write multiple times)
-	res.write("Amir Avidan"); // Send res body (can write multiple times)
+	res.writeHead(200);
+	res.write("Noam Roytman"); 
+	res.write("Amir Avidan"); 
 	res.write("Koral Tsaba");
-	res.end(); // Send & close connection
-}).listen(port); // Listen for connection on this port
+	res.write("Idan Kario");
+	res.end(); 
+}).listen(port); 
 
 console.log(`Listening on port ${port}`);
