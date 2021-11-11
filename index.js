2ded1e7c07f84533acf8ceadbbaf75e81b4bd61f
Koral_Tsaba <koraltsaba2@gmail.com>
2021-11-11 17:36:46 +0200

    koral Tsaba

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 8080;
 http.createServer((req, res) => {
 	res.writeHead(200); // Status code in header
 	res.write("Amir Avidan"); // Send res body (can write multiple times)
+	res.write("Koral Tsaba");
 	res.end(); // Send & close connection
 }).listen(port); // Listen for connection on this port
 
