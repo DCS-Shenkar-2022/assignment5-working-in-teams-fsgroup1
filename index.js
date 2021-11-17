b565ef20ccda21c7067b04e09ff375043c96ffbb
Koral Tsaba <79793566+KoralTsaba@users.noreply.github.com>
2021-11-17 15:00:30 +0200

    Update index.js
    delete somthing

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const port = process.env.PORT || 8080;
 app.get('/api/v1/githubUser/:githubUserName/avatar',(req,res) => {});
 app.get('/api/v1/githubUser/:githubUserName/repo/:repoName',(req,res) => {});
 app.get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers',(req,res) => {});
-
-http.createServer((req, res) => {
-	res.writeHead(200);
-	res.write("Koral Tsaba");
-	res.end(); 
-}).listen(port); 
+ 
 
 
 app.listen(port);
