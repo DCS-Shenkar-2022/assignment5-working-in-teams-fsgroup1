a1fa838104a96377b8a6998325263c3852354b91
BaderDak0 <baderdaka28@gmail.com>
2021-11-17 18:14:15 +0200

    Bader&Idan ex3+4

--- axiosmodel.js
+++ axiosmodel.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 //const url = axios.get(`https://api.github.com/users/${username}`);
-let username = 'BaderDak0';
-let username2= 'idankario'
+// let username = 'BaderDak0';
+// let username2= 'idankario'
 const getPrimise=(promise)=> {
     
     // using .then, create a new promise which extracts the data
@@ -10,23 +10,24 @@ const getPrimise=(promise)=> {
     // return it
     return dataPromise
 }
-const getGithubData=()=> {
+const getGithubData=(username)=> {
     // create a promise for the axios request
     return getPrimise(axios.get(`https://api.github.com/users/${username}`))
-
+    // /api/v1/github User/BaderDak0/avatar
 }
-const getGithubRepoName=()=> {
+const getGithubRepoName=(username)=> {
     // create a promise for the axios request
     return getPrimise(axios.get(`https://api.github.com/repos/${username}/Distributed-Cloud-Services-Web`));
 }
-const getRepoContributors=()=> {
+const getRepoContributors=(username)=> {
     // create a promise for the axios request
     return getPrimise(axios.get(`https://api.github.com/repos/${username2}/BookShoop-Cpp/contributors`))
 }
-
+// getGithubData('BaderDak0').then(e => console.log(e));
 module.exports = {
     getGithubData,  
     getGithubRepoName,
     getRepoContributors,
+    
   };
   
--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const {getGithubData,getGithubRepoName,getRepoContributors} =require('./axiosmodel')
-app.get('/api/v1/githubUser/:githubUserName/avatar',(req,res) => {});
-app.get('/api/v1/githubUser/:githubUserName/repo/:repoName',(req,res) => {});
-app.get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers',(req,res) => {});
-app.all('*',(req,res) => res.send('Global handler for all routes'));
-getGithubData().then(e=>console.log(e));
-getGithubRepoName().then(e=>console.log(e));
-getRepoContributors().then(e=>console.log(e));
+const { getGithubData, getGithubRepoName, getRepoContributors } = require('./axiosmodel')
+
+
+app.get('/api/v1/githubUser/:githubUserName/avatar', async (req, res) => {
+
+    let avatar = await getGithubData(req.params.githubUserName).then(e => e.avatar_url);
+    console.log(avatar);
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <img src=${avatar} title='avatar' alt='avatar'>
+
+    </body>
+    </html>`)
+
+
+});
+
+
+
+// app.get('/api/v1/githubUser/:githubUserName/repo/:repoName', (req, res) => {
+
+
+
+
+// });
+// app.get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers', (req, res) => {
+
+// });
+//  app.all('*', (req, res) => res.send('Global handler for all routes'));
+
+app.listen(port);
+// getGithubData().then(e => console.log(e));
+// getGithubRepoName().then(e => console.log(e));
+// getRepoContributors().then(e => console.log(e));
+
 
 
 
