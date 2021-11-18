660e77a16076bd03169a6f04b9115c166c906370
AmirAvidan <amiravidan0@gmail.com>
2021-11-18 19:03:29 +0200

    middleware added

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const http = require ("http");
-const port = 8080;
-http.createServer((req, res) => {
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const { getGithubData, getGithubRepoName, getRepoContributors } = require('./axiosmodel')
 
-	res.writeHead(200);
-	res.write("Idan Kario");
-	res.end(); 
-}).listen(port); 
 
-console.log(`Listening on port ${port}`);
+app.get('/api/v1/githubUser/:githubUserName/avatar', async (req, res) => {
+
+    let avatar = await getGithubData(req.params.githubUserName).then(e => e.avatar_url);
+    console.log(avatar);
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <img src=${avatar} title='avatar' alt='avatar'>
+
+    </body>
+    </html>`)
+
+
+});
+
+
+
+// app.get('/api/v1/githubUser/:githubUserName/repo/:repoName', (req, res) => {
+
+
+
+
+// });
+// app.get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers', (req, res) => {
+
+// });
+//  app.all('*', (req, res) => res.send('Global handler for all routes'));
+
+app.listen(port);
+// getGithubData().then(e => console.log(e));
+// getGithubRepoName().then(e => console.log(e));
+// getRepoContributors().then(e => console.log(e));
+
+
+
+
 
