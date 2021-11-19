9bca03ea6f5041996b08f8d29c9a22307126d5ca
noamroy <80029502+noamroy@users.noreply.github.com>
2021-11-19 09:19:18 +0200

    Final push

--- axiosmodel.js
+++ axiosmodel.js
@@ -1,36 +1,33 @@
 const axios = require('axios');
-
-//const url = axios.get(`https://api.github.com/users/${username}`);
-//let username = 'BaderDak0';
-// let username2= 'idankario'
+// const url = axios.get(`https://api.github.com/users/${username}`);
+// let username = 'BaderDak0';
+// let username = 'idankario';
+// let username = 'AmirAvidan';
+// let username = 'noamroy';
+// let username = 'Koral Tsaba';
+// let username = 'Michalglozman';
 const getPrimise=(promise)=> {
-    
     // using .then, create a new promise which extracts the data
-    const dataPromise =  promise.then((response) => response.data)
+    const dataPromise =  promise.then((response) => response.data);
     // return it
-    return dataPromise
+    return dataPromise;
 }
 const getGithubData=(username)=> {
     // create a promise for the axios request
-    return getPrimise(axios.get(`https://api.github.com/users/${username}`))
+    return getPrimise(axios.get(`https://api.github.com/users/${username}`));
     // /api/v1/github User/BaderDak0/avatar
 }
-const getGithubRepoName=(username,repo)=> {
+const getGithubRepoName=(username, repoName)=> {
     // create a promise for the axios request
-    return getPrimise(axios.get(`https://api.github.com/repos/${username}/${repo}`));
+    return getPrimise(axios.get(`https://api.github.com/repos/${username}/${repoName}`));
 }
-const getRepoContributors=(username,repo)=> {
+const getRepoContributors=(username,repoName)=> {
     // create a promise for the axios request
-    return getPrimise(axios.get(`https://api.github.com/repos/${username}/${repo}/contributors`))
+    return getPrimise(axios.get(`https://api.github.com/repos/${username}/${repoName}/contributors`));
 }
-//getGithubData('BaderDak0').then(e => console.log(e));
-//getGithubRepoName('BaderDak0').then(e => console.log(e));
-//getRepoContributors('BaderDak0').then(e => console.log(e));
 module.exports = {
     getGithubData,  
     getGithubRepoName,
     getRepoContributors,
-  };
+};
   
-
-//http://localhost:8080/api/v1/githubUser/BaderDak0/repo/Distributed-Cloud-Services-Web/
--- index.js
+++ index.js
@@ -1,79 +1,63 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-
 const { getGithubData, getGithubRepoName, getRepoContributors } = require('./axiosmodel')
-
-// app.param('githubUserName', (req,res,next,value) => {
-// 	if(/\d/.test(value) || /\s/.test(value)) {
-// 		res.send("Invaild input");
-// 	}
-// 	else
-// 		next();
-// });
-
-// app.param('repoName', (req,res,next,value) => {
-// 	if(/\d/.test(value) || /\s/.test(value)) {
-// 		res.send("Invaild input");
-// 	}
-// 	else
-// 		next();
-// });
-
+// ~~~~~~CHECK INOUT IS NOT NUMBER OR CONTAIN SPACES~~~~~~~
+app.param('githubUserName', (req,res,next,value) => {
+	if(/\d/.test(value) || /\s/.test(value)) {
+		res.send("Invaild input");
+	}
+	else
+		next();
+});
+app.param('repoName', (req,res,next,value) => {
+	if(/\d/.test(value) || /\s/.test(value)) {
+		res.send("Invaild input");
+	}
+	else
+		next();
+});
+// ~~~~~~~PRINT AVATAR
 app.get('/api/v1/githubUser/:githubUserName/avatar', async (req, res) => {
+    console.log(`got avatar request`);
     let avatar = await getGithubData(req.params.githubUserName).then(e => e.avatar_url);
-    console.log(avatar);
-    res.send(`<!DOCTYPE html>
+    //console.log(avatar);
+    res.send(`
+    <!DOCTYPE html>
     <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-    <img src=${avatar} title='avatar' alt='avatar'>
-
-    </body>
-    </html>`)
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <img src=${avatar} title='avatar' alt='avatar'>
+        </body>
+    </html>
+    `)
 });
-
+// ~~~~~~~PRINT Name
 app.get('/api/v1/githubUser/:githubUserName/repo/:repoName', async (req, res) => {
-    let rep_Name = await getGithubRepoName(req.params.githubUserName,req.params.repoName).then(e => e.name);
-    console.log(rep_Name);
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1> repo name: ${rep_Name} </h1>
-    </body>
-    </html>`)
+    console.log(`got name request`);
+    let repo = await getGithubRepoName(req.params.githubUserName,req.params.repoName).then(e => e);
+    res.setHeader("content-type","application/json");
+    res.send(JSON.stringify(repo,null,4));
 });
-
+// ~~~~~~~PRINT CONTRIBUTORS IN JSON
 app.get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers', async (req, res) => {
-    let rep_Contributers = await getRepoContributors(req.params.githubUserName,req.params.repoName).then(e => e.contributions);
-    console.log(rep_Contributers);
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <p> number of contributions: ${rep_Contributers} </p>
-    </body>
-    </html>`)
+    console.log(`got contributers request`);
+    let d =await getRepoContributors(req.params.githubUserName,req.params.repoName);
+    res.setHeader("content-type","application/json");
+    res.send(JSON.stringify(d,null,4));
 });
+// ~~~~~~~DEFAULT    
 app.all('*', (req, res) => res.send('Global handler for all routes'));
-
+console.log(`start listening port ${port}`);
 app.listen(port);
 // getGithubData().then(e => console.log(e));
 // getGithubRepoName().then(e => console.log(e));
 // getRepoContributors().then(e => console.log(e));
+
+
+
